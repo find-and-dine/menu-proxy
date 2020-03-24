b13d431f394720cc2825884192b14b4fe5f9629c
Sethaniel Way <seth@Sethaniels-MacBook-Pro.local>
2020-03-24 14:39:50 -0700

    created static html page with space for components to render

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -12,11 +12,39 @@ class App extends React.Component {
 
   render() {
     const { restaurantID } = this.state;
+    // eslint-disable-next-line no-console
+    console.log(restaurantID);
     return (
       <div>
-        Hello World,
-        {' '}
-        { restaurantID }
+        <div>
+          <img
+            className="dummyPhoto"
+            src="./resources/1.png"
+            alt="1"
+          />
+        </div>
+        <div
+          className="photoGallery"
+        />
+        <div>
+          <img
+            className="dummyPhoto"
+            src="./resources/2.png"
+            alt="2"
+          />
+        </div>
+        <div
+          id="menu"
+          className="menu"
+          restaurantID={restaurantID}
+        />
+        <div>
+          <img
+            className="dummyPhoto"
+            src="./resources/3.png"
+            alt="3"
+          />
+        </div>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.join(__dirname, 'client'),
+  entry: path.join(__dirname, 'client/index.jsx'),
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
