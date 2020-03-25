7fae851de8b1b0d6f65063bc0338c77837f371c7
Sethaniel Way <seth@Sethaniels-MacBook-Pro.local>
2020-03-25 16:52:10 -0700

    server up and running, renders 2 services from localhost (3002 and 3004)

--- client/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable react/jsx-filename-extension */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import path from 'path';
-import App from './components/App';
-
-ReactDOM.render(<App id={path.basename(window.document.URL)} />, document.getElementById('app'));
